cd5c61624c6c2d06112bd053aaaddea2ae002ce9
Minh Ngo <mkngo87@gmail.com>
2012-06-11 17:16:38 -0400

    removed comma in __call. bugfix to inverse method.

--- affine.lua
+++ affine.lua
@@ -14,7 +14,7 @@ local mt
 
 local __call = function (self,x,y)
 	return 	self[1][1]*x + self[1][2]*y + self[1][3],
-			self[2][1]*x + self[2][2]*y + self[2][3],
+			self[2][1]*x + self[2][2]*y + self[2][3]
 end
 
 local __mul = function (a,b)
@@ -64,18 +64,18 @@ end
 
 affine.inverse = function (u)
 	local t = setmetatable({},mt)
-	local det = u[1][1]*t[2][2] - u[1][2]*t[2][1]
-	assert(det ~= 0, 'transformation is not invertible!')
-	
 	local a = u[1][1]
 	local b = u[1][2]
 	local c = u[2][1]
 	local d = u[2][2]
+	
+	local det = a*d - b*c
+	assert(det ~= 0, 'transformation is not invertible!')
 	local f1 = ( d * u[1][3] + (-b) * u[2][3])/det
 	local f2 = (-c * u[1][3] +   a  * u[2][3])/det
 	
-	t[1] = { d/det, -b/det, f1}
-	t[2] = {-c/det,  a/det, f2}
+	t[1] = { d/det, -b/det, -f1}
+	t[2] = {-c/det,  a/det, -f2}
 	t[3] = {	 0,  	 0,  1}
 	return t
 end
